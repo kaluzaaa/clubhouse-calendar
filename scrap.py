0f5dca2f9deb1b919ab262484627e2d9a7a5ea31
Łukasz Kałużny <lukasz@kaluzny.pro>
2021-02-17 13:28:36 +0100

    Add logging to scraping

--- scrap.py
+++ scrap.py
@@ -83,4 +83,7 @@ airtable = Airtable(os.getenv('AT_BASE_ID'), 'Auditions', os.getenv('AT_API_KEY'
 for event in events:
     update = airtable.update_by_field('URL', event['url'], {'Hosts': ', '.join(event['speakers']), 'Description' : event['description'], 'Audition Name' : event['title']})
     if not update:
-        airtable.insert({'URL' : event['url'], 'Hosts': ', '.join(event['speakers']), 'Description' : event['description'], 'Audition Name' : event['title']})
+        insert = airtable.insert({'URL' : event['url'], 'Hosts': ', '.join(event['speakers']), 'Description' : event['description'], 'Audition Name' : event['title']})
+        print('Insert: ',insert)
+    else:
+        print('Update: ', update)
